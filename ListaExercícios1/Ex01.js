1cb3641968b725e2a277ad7b5fe3ebea977263f7
JaquePaula <jaquemarques.santos@gmail.com>
2024-04-22 13:38:01 -0300

    add Ex01

--- ListaExercícios1/Ex01.js
+++ ListaExercícios1/Ex01.js
@@ -1,1 +1,10 @@
 // exercicio 1 
+// Escreva um algoritmo para ler uma temperatura em graus Celsius, calcular e escrever o valor correspondente em graus Fahrenheit. 
+//  F = C x 1,8 + 32
+
+const prompt = require("prompt-sync")();
+
+let C = prompt ("Digite o numero em Celsius para conversão em Fahrenheit: ")
+let F = (C * 1.8 + 32);
+console.log(C, "temperatura em Celsius e", F, "temperatura em Fahrenheit")
+
